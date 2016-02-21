cea38c3cae815e91163bb2dd310b4cbd6d7bb595
oigami <oigamishunta@gmail.com>
2016-02-21 21:06:07 +0900

    Cupsuleクラスの追加

---
+++ Siv3D_MMD/BulletPhysics/BulletCapsule.cpp
@@ -0,0 +1,29 @@
+#include "BulletCapsule.h"
+#include "BulletCollision/CollisionShapes/btCapsuleShape.h"
+
+namespace s3d_bullet {
+  namespace bullet {
+    class Capsule::Pimpl {
+    public:
+      float m_mass;
+      std::shared_ptr<btCapsuleShape> shape;
+
+      Pimpl(float radius, float height) {
+        shape = std::make_shared<btCapsuleShape>(radius, height);
+      }
+    };
+
+    Capsule::Capsule(float radius, float height) {
+      m_pimpl = std::make_shared<Pimpl>(radius, height);
+    }
+
+    void Capsule::setMass(float mass) {
+      m_pimpl->m_mass = mass;
+    }
+
+    Capsule::operator Shape() const {
+      return Shape(m_pimpl->shape, m_pimpl->m_mass);
+    }
+
+  }
+}
