d8c46377f4ca6b47006888ca1bd263f0961671b8
oigami <oigamishunta@gmail.com>
2016-02-21 19:25:48 +0900

    sphereクラスの追加

---
+++ Siv3D_MMD/BulletPhysics/BulletSphere.cpp
@@ -0,0 +1,29 @@
+#include "BulletSphere.h"
+#include "BulletCollision/CollisionShapes/btSphereShape.h"
+
+namespace s3d_bullet {
+  namespace bullet {
+    class Sphere::Pimpl {
+    public:
+      float m_mass;
+      std::shared_ptr<btSphereShape> shape;
+
+      Pimpl(float radius) {
+        shape = std::make_shared<btSphereShape>(radius);
+      }
+    };
+
+    Sphere::Sphere(float radius) {
+      m_pimpl = std::make_shared<Pimpl>(radius);
+    }
+
+    void Sphere::setMass(float mass) {
+      m_pimpl->m_mass = mass;
+    }
+
+    Sphere::operator Shape() const {
+      return Shape(m_pimpl->shape, m_pimpl->m_mass);
+    }
+
+  }
+}
---
+++ Siv3D_MMD/BulletPhysics/BulletSphere.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <memory>
+#include "BulletRigidBody.h"
+namespace s3d_bullet {
+  namespace bullet {
+    class Sphere {
+      class Pimpl;
+      std::shared_ptr<Pimpl> m_pimpl;
+
+    public:
+
+      Sphere() = default;
+      Sphere(float radius);
+      void setMass(float mass);
+      operator Shape()const;
+    };
+  }
+}
